4588848485f7a69bb125b0838c123ab3c8183ec7
Lucas Lopes <lucaslopes.ti@live.com>
2026-01-01 19:09:52 -0300

    verssão de aplicativo android buld realizado com sucesso

---
+++ android/app/src/main/java/br/com/ipbocian/biblia365/DelegationService.java
@@ -0,0 +1,14 @@
+package br.com.ipbocian.biblia365;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
